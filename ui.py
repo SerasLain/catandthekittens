a6a2abaa0bb46a40551b6eb7243e30808948589a
ABaranchikova <six3.danika@gmail.com>
2018-03-29 16:50:28 +0300

    GUI works

--- ui.py
+++ ui.py
@@ -17,44 +17,54 @@ class Example(QWidget):
                                             'How many metadata to show?')
 
             if ok:
+                print('alive')
                 return number
 
-        elif flag == 2:
+        if flag == 2:
             text, ok = QInputDialog.getText(self, 'CAT articles',
                                             'What author?')
 
             if ok:
                 return text
 
-        elif flag == 3:
+        if flag == 3:
             text, ok = QInputDialog.getText(self, 'CAT articles',
                                             'What part of speech?')
 
             if ok:
                 return text
 
-    def connect_to_db(self, flag, cur):
-
+    def connect_to_db(self, flag):
+        con = mysql.connect(host='127.0.0.1', database='cat_scheme', user='root', password='')
+        cur = con.cursor(dictionary=True)
+        data_list = []
         if flag == 1:
+            print('still alive')
             cur.execute("SELECT * FROM meta_cat LIMIT 0, {0}".format(self.showDialog(flag)))
+            data = cur.fetchall()
+            data_list = list(data[0].values())
         elif flag == 2:
             cur.execute("SELECT word FROM words_cat WHERE id_text = (SELECT id_text FROM meta_cat"
                         " WHERE author LIKE '%{0}%')".format(self.showDialog(flag)))
+            data = cur.fetchall()
+            for d in data:
+                data_list+=list(d.values())
         elif flag == 3:
             cur.execute("SELECT word FROM words_cat WHERE POS_tag LIKE '{0}' LIMIT 10".format(self.showDialog(flag)))
-        data = cur.fetchall()
-        data_list = list(data[0].values())
+            data = cur.fetchall()
+            for d in data:
+                data_list += list(d.values())
         for item in data_list:
             self.textBox.append(str(item))
 
     def handleButton1(self):
-        self.connect_to_db(1,self.cur)
+        self.connect_to_db(1)
 
     def handleButton2(self):
-        self.connect_to_db(2,self.cur)
+        self.connect_to_db(2)
 
     def handleButton3(self):
-        self.connect_to_db(3,self.cur)
+        self.connect_to_db(3)
 
     def initUI(self):
         QToolTip.setFont(QFont('SansSerif', 10))
@@ -63,12 +73,11 @@ class Example(QWidget):
 
         self.textBox = QTextEdit(self)
         self.textBox.move(0, 30)
-        self.textBox.resize(355,270)
+        self.textBox.resize(400,370)
 
-        con = mysql.connect(host='127.0.0.1', database='cat_scheme', user='root', password='')
-        self.cur = con.cursor(dictionary=True)
 
-        btn = QPushButton('Show article metadata', self)
+
+        btn = QPushButton('Show metadata', self)
         btn.setToolTip('This is a <b>QPushButton</b> widget')
         btn.resize(btn.sizeHint())
         btn.clicked.connect(self.handleButton1)
@@ -83,7 +92,7 @@ class Example(QWidget):
         btn2.move(240, 0)
         btn2.clicked.connect(self.handleButton3)
 
-        self.setGeometry(300, 300, 355, 300)
+        self.setGeometry(300, 300, 400, 400)
         self.setWindowTitle('CAT')
         self.show()
 
