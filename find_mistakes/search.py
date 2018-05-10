ce8515436cdd2c47f87b0c33af05bf6fabff1b3a
MariaFjodorowa <laks.salmon@gmail.com>
2018-05-10 04:25:12 +0300

    C-10, C-11 [+] : поиск ошибок более лучше и шел дождь и рота солдат

--- find_mistakes/search.py
+++ find_mistakes/search.py
@@ -2,6 +2,7 @@ import argparse
 from conllu import parse
 from find_mistakes.search_for_shit import Searcher
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='file to check')
@@ -9,17 +10,20 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
 def parse_file(file):
     with open(file, 'r', encoding='utf-8') as f:
         data = f.read()
         tree = parse(data)[0]
     return tree
 
+
 if __name__ == '__main__':
     args = parse_args()
     file = args.file
     tree = parse_file(file)
-    #threshold = args.threshold
+    # threshold = args.threshold
     searcher = Searcher()
-    searcher.find_wrong_comparativ(tree)
-    print(searcher.found)
+    searcher.find_wrong_coordinate_NPs(tree)
+    for x in searcher.found['coordinate_NPs']:
+        print(x)
---
+++ find_mistakes/search_for_shit.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+from gensim.models import Word2Vec
+
+
+class Searcher:
+    def __init__(self):
+        self.found = defaultdict(list)
+
+    def find_genitives(self, tree, file, threshold):
+        gen_chain = []
+        for i in range(len(tree)):
+            line = tree[i]
+            if 'gen' in line['feats']:
+                gen_chain.append((line['form'], i))
+            else:
+                if len(gen_chain) >= int(threshold):
+                    self.found['genitives'].append(gen_chain)
+                gen_chain = []
+        with open("gen.{}.txt".format(file).replace('/', '_'), 'w', encoding='utf-8') as f:
+            for ch in self.found['genitives']:
+                for g in ch:
+                    f.write(g[0] + ' ')
+                f.write('\r\n')
+
+    def find_wrong_comparativ(self, tree):
+        more_less = ['более', 'менее']
+        for i in range(len(tree) - 1):
+            line = tree[i]
+            next = tree[i + 1]
+            if line['form'] in more_less and 'comp' in next['feats']:
+                self.found['comparatives'].append((line['form'], next['form'], i))
+
+    def find_wrong_coordinate_NPs(self, tree):
+        model = Word2Vec.load('../collocation_frequences/Models/LinguisticModel')
+        for i in range(1, len(tree) - 1):
+            if tree[i]['form'] == 'и':
+                t = i
+                pair = []
+                while 'S' not in tree[t]['feats'] and 'V' not in tree[t]['feats'] and t > 0:
+                    t -= 1
+                if 'S' in tree[t]['feats']:
+                    pair.append(tree[t]['form'])
+                t = i
+                while 'S' not in tree[t]['feats'] and 'V' not in tree[t]['feats'] and t < len(tree):
+                    t += 1
+                if 'S' in tree[t]['feats']:
+                    pair.append(tree[t]['form'])
+                if len(pair) > 1:
+                    if pair[0] in model.wv.vocab and pair[1] in model.wv.vocab:
+                        self.found['coordinate_NPs'].append(pair + [i, model.similarity(pair[0], pair[1])])
+                    else:
+                        self.found['coordinate_NPs'].append(pair + [i, float('-inf')])
