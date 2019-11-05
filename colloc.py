650b9be3d57c386dd62179fc438c28c5b1bba8c7
SerasLain <seraslain@gmail.com>
2019-11-05 13:52:43 +0300

    some minor changes

--- colloc.py
+++ colloc.py
@@ -1,5 +1,69 @@
 import os
 from math import log10, sqrt, log2
+import logging
+
+
+def filter_by_tsc(path):
+    with open(path, 'r', encoding='utf-8') as s:
+        print(path)
+        with open(path[:-4]+'_filtered.csv', 'a', encoding='utf-8') as t:
+            for line in s.readlines():
+                if line.startswith("coll"):
+                    t.write(line)
+                elif float(line.split('\t')[4]) > 1.00:
+                    t.write(line)
+                else:
+                    pass
+
+
+def count_567(domain_path):
+    # Just script to get ngrams from the corpus
+    unigrams = read_ngrams(os.path.join(domain_path, '1.csv'))
+    corpus_size = sum([int(i) for i in unigrams.values()])
+
+    quadrigrams = {}
+    with open(os.path.join(domain_path, '4_scored_ranked_logDice.csv'), 'r', encoding='utf-8') as f:
+        # collocation_tags	raw_frequency	log_Dice	PMI	tscore	logDice rank	pmi_rank	tsc_rank	summary pmi and tsc	summary logDice and pmi
+        for line in f.readlines():
+            line_arr = line.strip('\n').split('\t')
+            collocation_tags, word_freq = line_arr[0], line_arr[1]
+            quadrigrams[collocation_tags] = word_freq
+
+    n1grams = quadrigrams
+    for i in range(5, 8):
+        if i == 5:
+            count_ngram(domain_path, str(i) + '.csv', str(i)+'_scored.csv', n1grams, unigrams, corpus_size)
+        else:
+            count_ngram(domain_path, str(i) + '.csv', str(i)+'_scored.csv', str(i-1)+'.csv', unigrams, corpus_size)
+
+
+
+
+def count_ngram(domain_path, ngram_raw, ngram_scored, n1grams, unigrams, corpus_size):
+    # One more script to count all metrics for 5,6,7-grams
+    if type(n1grams) is str:
+        n1grams = read_ngrams(os.path.join(domain_path, n1grams))
+    else:
+        pass
+    print(ngram_raw)
+    print(list(n1grams.keys())[:3])
+    quingrams = read_ngrams(os.path.join(domain_path, ngram_raw))
+    quingrams_scored = open(os.path.join(domain_path, ngram_scored), 'a', encoding='utf-8')
+    print('head')
+    quingrams_scored.write('\t'.join(['collocation and tags', 'raw frequency', 'log-Dice', 'PMI', 't-score']) + '\n')
+    for colloc_tag in quingrams:
+        try:
+            logdsc, pmisc, tsc = measure(colloc_tag, quingrams[colloc_tag],
+                                         n1grams, unigrams, corpus_size)
+            quingrams_scored.write('\t'.join([str(i) for i in [colloc_tag, quingrams[colloc_tag], logdsc, pmisc, tsc]])
+                                   + '\n')
+        except KeyError:
+            print(colloc_tag)
+    quingrams_scored.close()
+    print('done')
+    return quingrams
+
+
 
 
 def domain_counts(domain_path):
@@ -153,19 +217,23 @@ def read_ngrams_scored(path):
     return ngrams
 
 
-def ranked(pmi_ranking, tscore_ranking, tscore, PMI):
+def ranked(pmi_ranking, tscore_ranking, logdice_ranking, tscore, PMI, logdice):
     """
     counts ranks for the collocation
     :param pmi_ranking: list of all pmi scores, sorted from more to less
     :param tscore_ranking: list of all t-scores in from more to less
+    :param logdice_ranking: list of all logDice scores, sorted from more to less
     :param tscore: float, t-score
     :param PMI: float, PMI score
+    :param logdice: float, logDice score
     :return: tuple of floats
     """
     tsc_rank = tscore_ranking[(float(tscore))]
     pmi_rank = pmi_ranking[float(PMI)]
-    summary = tsc_rank + pmi_rank
-    return tsc_rank, pmi_rank, summary
+    logdice_rank = logdice_ranking[float(logdice)]
+    summary_tscpmi = tsc_rank + pmi_rank
+    summary_logdpmi = logdice_rank + pmi_rank
+    return tsc_rank, pmi_rank, logdice_rank, summary_tscpmi, summary_logdpmi
 
 def get_rank(ngrams_set, path):
     """
@@ -178,21 +246,30 @@ def get_rank(ngrams_set, path):
     pmi_ranking = {}
     for i in range(len(pmi_sorted)):
         pmi_ranking[pmi_sorted[i]] = i + 1
+
     tscore_sorted = sorted(set([float(tup[4]) for tup in ngrams_set]), reverse=True)
     tscore_ranking = {}
     for i in range(len(tscore_sorted)):
         tscore_ranking[tscore_sorted[i]] = i + 1
+
+    logDice_sorted = sorted(set([float(tup[2]) for tup in ngrams_set]), reverse=True)
+    logDice_ranking = {}
+    for i in range(len(logDice_sorted)):
+        logDice_ranking[logDice_sorted[i]] = i + 1
+
     with open(path[:-4] + '_ranked.csv', 'a', encoding='utf-8') as f:
         f.write('\t'.join(['collocation_tags', 'raw_frequency', 'log_Dice', 'PMI', 'tscore',
-                           'pmi_rank', 'tsc_rank', 'summary']) + '\n')
+                           'pmi_rank', 'tsc_rank', 'logD_rank', 'summary t-sc + pmi', 'summary logD + pmi']) + '\n')
         for collocation_scored in ngrams_set:
             collocation_tags, raw_frequency, log_Dice, PMI, tscore = collocation_scored
-            tsc_rank, pmi_rank, summary = ranked(pmi_ranking, tscore_ranking, tscore, PMI)
+            tsc_rank, pmi_rank, logd_rank, summarytscpmi, summarylogdpmi = ranked(pmi_ranking, tscore_ranking,
+                                                                                  logDice_ranking, tscore, PMI, log_Dice)
             f.write('\t'.join([collocation_tags, raw_frequency,
-                               log_Dice, PMI, tscore, str(pmi_rank), str(tsc_rank), str(summary)]) + '\n')
+                               log_Dice, PMI, tscore, str(pmi_rank), str(tsc_rank), str(logd_rank),
+                               str(summarytscpmi), str(summarylogdpmi)]) + '\n')
 
 
-# These two functions should be add into the previous one!
+# These two functions added into the previous one
 
 def logD_ranked(logDice_ranking, logD, pmi_rank):
     """
@@ -232,21 +309,34 @@ def get_logd_rank(ngrams_set, path):
 
 
 def main():
-    domains = os.listdir(r'C:\Users\Eiko\Documents\Domains_collocations\Domains_collocations')
-    for name in domains:
-        print(name)
-        domain_counts(os.path.join(r"C:\Users\Eiko\Documents\Domains_collocations\Domains_collocations\\", name))
-        domain = os.path.join(r"C:\Users\Eiko\Documents\Domains_collocations\Domains_collocations\\", name)
-        print(domain)
-        scored = os.listdir(domain)
-        fls = [fl for fl in scored if ('scored.csv' in fl)]
-        # Here shoild be the code for ranked without logDice, but I hope if it would be really necessary to do it again,
-        # I will write a func, which counts all rankings
-        for f in fls:
-            path = os.path.join(domain, f)
-            print(path)
-            ngrams = read_ngrams_scored(path)
-            get_logd_rank(ngrams, path)
+    root = r'C:\Users\Eiko\Documents\Domains_collocations\Domains_collocations'
+    #domains = os.listdir(root)
+    #for name in domains:
+    for name in ["Sociology and History"]:
+        content = tuple(os.listdir(os.path.join(root, name)))
+        fls = [tsv for tsv in content if ("ranked" in tsv and "filtered" not in tsv)]
+        for tsv in fls:
+            filter_by_tsc(os.path.join(os.path.join(root, name, tsv)))
+
+        # count_567(os.path.join(root, name))
+
+
+    #     domain_counts(os.path.join(r"C:\Users\Eiko\Documents\Domains_collocations\Domains_collocations\\", name))
+    #     domain = os.path.join(r"C:\Users\Eiko\Documents\Domains_collocations\Domains_collocations\\", name)
+    #     print(domain)
+    #     scored = os.listdir(domain)
+    #     fls = [fl for fl in scored if ('scored.csv' in fl)]
+    # #     # Here should be the code for ranks
+    #     #     d without logDice, but I hope if it would be really necessary to do it again,
+    # #     # I will write a func, which counts all rankings
+    #
+    # #     # Yep I did it
+    #
+    #     for f in fls:
+    #         path = os.path.join(domain, f)
+    #         print(path)
+    #         ngrams = read_ngrams_scored(path)
+    #         get_rank(ngrams, path)
 
 
 if __name__ == "__main__":
